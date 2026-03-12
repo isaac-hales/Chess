f280fe2e0b9e20ad9ef91d02958e225eeba764a7
Isaac Hales <isaachales64@gmail.com>
2026-03-11 18:18:43 -0600

    Finished SQLUserDAO, and started SQLAuthDAO

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,4 +1,56 @@
 package dataaccess;
 
+import chess.AuthData;
+import chess.UserData;
+
+import java.sql.SQLException;
+
 public class SQLAuthDAO {
+
+    public AuthData createAuth(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO auth ")) {
+                preparedStatement.setString(1,username);
+                AuthData newAuth = AuthData.create(username);
+                var results = preparedStatement.executeUpdate();
+                if (results.next()) {
+                    return newAuth;
+                }
+                return null; //User is not found.
+            }
+        }
+        catch (SQLException e) {
+            throw new DataAccessException("error message", e);
+        }
+    }
+
+    public AuthData getAuth(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT username, authorization FROM authTokens WHERE username = ?")) {
+                preparedStatement.setString(1,username);
+                var results = preparedStatement.executeQuery();
+                if (results.next()) {
+                    return new AuthData(results.getString("username"),
+                            results.getString("authorization"));
+                }
+                return null; //User is not found.
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("error message", e);
+        }
+    }
+
+    public void deleteAuth(String username) throws DataAccessException {
+
+    }
+
+    public void clearAuthTokens() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("TRUNCATE TABLE auth")) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("error message", e);
+        }
+    }
 }
--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,4 +1,50 @@
 package dataaccess;
 
+import chess.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.SQLException;
+
 public class SQLUserDAO {
+    public void createUser(UserData user) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO users (username, email, hashedPassword) VALUES(?, ?, ?)")) {
+                String password = user.password();
+                String hashPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+                preparedStatement.setString(1, user.username());
+                preparedStatement.setString(2, user.email());
+                preparedStatement.setString(3,hashPassword);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("error message", e);
+        }
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT username, email, hashedPassword FROM users WHERE username = ?")) {
+                preparedStatement.setString(1,username);
+                var results = preparedStatement.executeQuery();
+                if (results.next()) {
+                    return new UserData(results.getString("username"),
+                                        results.getString("hashedPassword"),
+                                        results.getString("email"));
+                }
+                return null; //User is not found.
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("error message", e);
+        }
+    }
+
+    public void clearUsers() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("TRUNCATE TABLE users")) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("error message", e);
+        }
+    }
 }
