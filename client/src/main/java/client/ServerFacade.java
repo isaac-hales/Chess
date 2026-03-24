502574a39bc9d025622959eb06f9424b3cf8054e
Isaac Hales <isaachales64@gmail.com>
2026-03-24 14:57:48 -0600

    Worked on and completed a helper function, since the same code will be reused with each request. I next work on listGames

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,46 +1,72 @@
 package client;
 
 import chess.AuthData;
+import chess.ChessGame;
+import chess.GameData;
 import chess.UserData;
+import com.google.gson.Gson;
 
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 
 public class ServerFacade {
     private final String serverUrl;
+    private final Gson gson = new Gson();
+    private record CreateGameRequest(String gameName) {}
+    private record CreateGameResult(int gameID) {}
 
     public ServerFacade(int port) {
         serverUrl = "http://localhost:" + port;
+
     }
 
-    private <T> T makeRequest(String method, String path, Object requestBody, Class<T> responseClass, String authToken) {
-        var url = new URI(serverUrl + "/" + path).toURL();
+    private <T> T makeRequest(String method, String path, Object requestBody, Class<T> responseClass, String authToken) throws Exception{
+        var url = new URI(serverUrl + path).toURL();
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod(method);
-        //I don't really know what to put here
-        connection.setRequestProperty();
+        connection.setRequestProperty("Content-Type", "application/json");
+        if (authToken != null) {
+            connection.setRequestProperty("Authorization", authToken);
+        }
         connection.setDoOutput(true);
-        try (var outputStream = connection.getOutputStream()) {
 
+        if (requestBody != null) {
+            try (var outputStream = connection.getOutputStream()) {
+                var json = gson.toJson(requestBody);
+                outputStream.write(json.getBytes());
+            }
         }
-        try (var inputStream = connection.getInputStream()) {
-
+        if (responseClass != null) {
+            try (var inputStream = connection.getInputStream()) {
+                return gson.fromJson(new InputStreamReader(inputStream), responseClass);
+            }
         }
-        return;
+        return null;
     }
 
-    public AuthData register(String username, String password, String email) {
+    public AuthData register(String username, String password, String email) throws Exception {
         UserData tempUser = new UserData(username,password,email);
         return makeRequest("POST","/user",tempUser, AuthData.class, null);
     }
 
-    public AuthData login(String username, String password){
+    public AuthData login(String username, String password) throws Exception {
         UserData tempUser = new UserData(username, password, null);
         return makeRequest("POST", "/session", tempUser, AuthData.class,null);
     }
 
+    public void logout(String authToken) throws Exception {
+        makeRequest("DELETE","/session", null, null, authToken);
+    }
 
+    public int createGame(String gameName, String authToken) throws Exception {
+        var request = new CreateGameRequest(gameName);
+        var result = makeRequest("POST", "/game", request, CreateGameResult.class, authToken);
+        return result.gameID();
+    }
 }
+
+
 /***
  * // 1. Build the URL
  * var url = new URI(serverUrl + "/user").toURL();
