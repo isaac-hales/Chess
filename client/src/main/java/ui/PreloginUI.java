e38d947783cbe61db6fd2e134e925051d73d0558
Isaac Hales <isaachales64@gmail.com>
2026-03-25 11:05:15 -0600

    Working on prelogin ui. Created 3 new files for it.

--- client/src/main/java/ui/PreloginUI.java
+++ client/src/main/java/ui/PreloginUI.java
@@ -1,4 +1,28 @@
 package ui;
+import client.ServerFacade;
 
 public class PreloginUI {
+    private final ServerFacade facade;
+    private String authToken = null;
+    private String newline = "";
+
+    public PreloginUI(ServerFacade facade) {
+        this.facade = facade;
+    }
+
+    public String eval(String input) {
+        var cmd = input.toLowerCase().trim();
+        switch (cmd) {
+            case "help" -> { return "register <USERNAME> <PASSWORD> <EMAIL> - to create an account\n" +
+                    "login <USERNAME> <PASSWORD> - to play chess\n" +
+                    "quit - playing chess\n" +
+                    "help - with possible commands\n"; }
+            case "quit" -> { return "quit"; }
+            case "login" -> { }
+            case "register" -> { }
+            default -> { return "Unknown command. Type 'help' for options."; }
+        }
+    }
+
+
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,31 @@
 package ui;
 
+import client.ServerFacade;
+import java.util.Scanner;
+import java.util.Scanner;
+
 public class Repl {
+    private final ServerFacade facade;
+    private String authToken = null;
+
+    public Repl(ServerFacade facade) {
+        this.facade = facade;
+    }
+
+    public void run() {
+        System.out.println("Welcome to 240 chess. Type Help to get started.");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            String userInput = scanner.nextLine();
+            if (authToken == null) {
+                //PreLogin Stuff
+            }
+            else {
+                //PostLogin Stuff
+            }
+        }
+    }
+
+
 }
